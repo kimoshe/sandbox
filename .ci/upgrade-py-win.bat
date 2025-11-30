8b2d7f43142df96e008021648fda289d39488764
kimoshe <116674739+kimoshe@users.noreply.github.com>
2025-11-30 09:29:37 -0800

    fix error

--- .ci/upgrade-py-win.bat
+++ .ci/upgrade-py-win.bat
@@ -1,15 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 ::
-:: Upgrade the Python version to PYUPGRADE_WIN_V whenever the environment variable exists
+:: Upgrade the Python version to PYUPGRADE_WIN_VER whenever the environment variable exists
 ::   and the upgrade version is greater than current Python version. 
 ::
 
 :: If there is no upgrade version then simply return
-::if "%PYUPGRADE_WIN_V%"=="" (
+::if "%PYUPGRADE_WIN_VER%"=="" (
 ::    goto End
 ::)
-
+::
 :: Check that python.exe exists, if not go straight to the upgrade. 
 :: Note: goto used to avoid comments in nested brackets issues.  Better to have comments.
 ::if Not Exist "%PYTHON_PATH%\python.exe" (
@@ -18,59 +18,63 @@ setlocal enabledelayedexpansion
 ::)
 
 :: Get the current Python version if python.exe exists
-if exist %PREV_PYTHON_PATH%\python.exe
-(
+if exist "%PREV_PYTHON_PATH%\python.exe" (
     for /f "tokens=2 delims= " %%a in ('%PREV_PYTHON_PATH%\python.exe -V 2^>^&1') do set "PREV_PYTHON_VER=%%a"
-) else (
+    ) else (
     echo **** ERROR: %PREV_PYTHON_PATH%\python.exe does not exist in the build environment
     exit /b 91
 )
 
-echo *** Current Python Version: %PREV_PYTHON_VER%  Upgrade to: %PYUPGRADE_WIN_V% requested
+echo *** Current Python Version: !PREV_PYTHON_VER!  Upgrade to: !PYUPGRADE_WIN_VER! requested
+
+if "!PREV_PYTHON_VER!" == "!PYUPGRADE_WIN_VER!" (
+    echo *** Versions are the same.  No need to upgrade.
+    goto End
+)
 
 :: Split the version strings into components - major.minor.patch
-for /f "tokens=1,2,3 delims=." %%a in ("%PREV_PYTHON_VER%") do (
+for /f "tokens=1,2,3 delims=." %%a in ("!PREV_PYTHON_VER!") do (
     set "major_py=%%a"
     set "minor_py=%%b"
     set "patch_py=%%c"
 )
-for /f "tokens=1,2,3 delims=." %%a in ("%PYUPGRADE_WIN_V%") do (
+for /f "tokens=1,2,3 delims=." %%a in ("!PYUPGRADE_WIN_VER!") do (
     set "major_up=%%a"
     set "minor_up=%%b"
     set "patch_up=%%c"
 )
 
 :: Compare version numbers at each level
-if %major_py% lss %major_up% (
+if !major_py! lss !major_up! (
     goto Upgrade
-) else if %major_py% gtr %major_up% (
+) else if !major_py! gtr !major_up! (
     goto NoUpgrade
 )
-if %minor_py% lss %minor_up% (
+if !minor_py! lss !minor_up! (
     goto Upgrade
-) else if %minor_py% gtr %minor_up% (
+) else if !minor_py! gtr !minor_up! (
     goto NoUpgrade
 )
-if %patch_py% lss %patch_up% (
+if !patch_py! lss !patch_up! (
     goto Upgrade
 )
 goto NoUpgrade
 
 :Upgrade
-echo ***** Upgrading Python from %PYTHON_V% to %PYUPGRADE_WIN_V%
+echo ***** Upgrading Python from !PREV_PYTHON_VER! to !PYUPGRADE_WIN_VER!
 echo *** Downloading Python install exe
-curl -L -O https://www.python.org/ftp/python/%PYUPGRADE_WIN_V%/python-%PYUPGRADE_WIN_V%-amd64.exe
-if not exist python-%PYUPGRADE_WIN_V%-amd64.exe (exit /b 80)
-echo *** Installing Python %PYUPGRADE_WIN_V%
-python-%PYUPGRADE_WIN_V%-amd64.exe /quiet PrependPath=1
-if not exist %PYTHON_PATH%\python.exe (exit /b 90)
+curl -L -O https://www.python.org/ftp/python/!PYUPGRADE_WIN_VER!/python-!PYUPGRADE_WIN_VER!-amd64.exe
+if not exist python-!PYUPGRADE_WIN_VER!-amd64.exe (exit /b 80)
+echo *** Installing Python !PYUPGRADE_WIN_VER!
+python-!PYUPGRADE_WIN_VER!-amd64.exe /quiet PrependPath=1
+if not exist !PYTHON_PATH!\python.exe (exit /b 90)
 echo ***** Upgrade Complete
 echo Python Version Now:
 python -V
 goto End
 
 :NoUpgrade
-echo **** ERROR: Python upgrade not happening from %PREV_PYTHON_VER% to %PYUPGRADE_WIN_V%
+echo **** ERROR: Python upgrade not happening from !PREV_PYTHON_VER! to !PYUPGRADE_WIN_VER!
 exit /b 92
 
 :End
