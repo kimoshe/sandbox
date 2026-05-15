35041a30936e78bed47cdf0ebeed8c67a3b0ea36
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-15 11:29:55 -0700

    correct logic

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '4.0.3'
+__version__ = '4.99.7'
 __revision__ = ''
 __build__ = '0'
 __signature__ = ''
--- src/artisanlib/main.py
+++ src/artisanlib/main.py
@@ -18013,7 +18013,10 @@ class ApplicationWindow(QMainWindow):
                 _log.info(f"*** {QVersionNumber(4,2,0)=}")  #dave #TODO
 
                 if (self.official_build and not debugLogLevelActive() and
-                        settings.contains('System/artisan_version') and QVersionNumber.fromString(settings.value('artisan_version',__version__))[0] >= QVersionNumber(4,2,0)):
+                        settings.contains('System/artisan_version') and 
+                        QVersionNumber.fromString(settings.value('System/artisan_version')) >= QVersionNumber(4,2,0) and
+                        QVersionNumber.fromString(settings.value('artisan_version',__version__))[0] >= QVersionNumber(4,2,0)):
+:
                     _log.info('*** Inside') #dave #TODO
 #                # testing:
 #                if (self.official_build and not debugLogLevelActive() and
