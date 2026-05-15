3d8e4e49c99eed9ec531b993ad98734f6c9591d1
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-14 22:53:20 -0700

    Update main.py

--- src/artisanlib/main.py
+++ src/artisanlib/main.py
@@ -18009,7 +18009,7 @@ class ApplicationWindow(QMainWindow):
 
                 if (self.official_build and not debugLogLevelActive() and
                         settings.contains('artisan_version') and QVersionNumber.fromString(settings.value('artisan_version',__version__))[0] >= QVersionNumber(4,1,0)):
-                _log.info('Inside') #dave #TODO
+                    _log.info('Inside') #dave #TODO
 #                # testing:
 #                if (self.official_build and not debugLogLevelActive() and
 #                        settings.contains('System/artisan_version') and settings.contains('System/artisan_signature')):
