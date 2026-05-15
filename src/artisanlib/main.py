de7167df14c2fab4c4fe3fb60f224ab87d451fe6
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-15 11:51:03 -0700

    Update main.py

--- src/artisanlib/main.py
+++ src/artisanlib/main.py
@@ -18013,8 +18013,8 @@ class ApplicationWindow(QMainWindow):
                 _log.info(f"*** {QVersionNumber(4,2,0)=}")  #dave #TODO
 
                 if (self.official_build and not debugLogLevelActive() and
-                        settings.contains('System/artisan_version') and 
-                        QVersionNumber.fromString(settings.value('System/artisan_version')) >= QVersionNumber(4,2,0) and
+#                        settings.contains('System/artisan_version') and 
+                        QVersionNumber.fromString(settings.value('System/artisan_version'))[0] >= QVersionNumber(4,2,0) and
                         QVersionNumber.fromString(settings.value('artisan_version',__version__))[0] >= QVersionNumber(4,2,0)):
 :
                     _log.info('*** Inside') #dave #TODO
