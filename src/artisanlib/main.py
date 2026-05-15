ff0a80664ff1d88d23516aa30d2c165322d0c01e
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-14 22:42:18 -0700

    Update main.py

--- src/artisanlib/main.py
+++ src/artisanlib/main.py
@@ -18005,7 +18005,7 @@ class ApplicationWindow(QMainWindow):
 
                 # check signature if official build and settings 'artisan_version' is >= 4.1
                 _log.info(f"{self.official_build=}, {debugLogLevelActive()}, {settings.contains('artisan_version')},"  #dave #TODO
-                _log.info(f"{QVersionNumber.fromString(settings.value('artisan_version',__version__))[0] >= QVersionNumber(4,1,0))}") #dave #TODO
+                _log.info(f"{QVersionNumber.fromString(settings.value('artisan_version',__version__))[0] >= QVersionNumber(4,1,0)}") #dave #TODO
 
                 if (self.official_build and not debugLogLevelActive() and
                         settings.contains('artisan_version') and QVersionNumber.fromString(settings.value('artisan_version',__version__))[0] >= QVersionNumber(4,1,0)):
