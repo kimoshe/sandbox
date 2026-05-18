b989b04b47bf5484ead9782f9bcc181bdc3742a5
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-18 16:20:44 -0700

    from gikraken

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '4.0.3'
-__revision__ = '3'
+__revision__ = '1'
 __build__ = '0'
 __artisan_os__ = 'Linux'
 
 __release_sponsor_name__ = 'artisan.plus'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
-__signature__ = ''
+__signature__ = 'ab04f02a9e8b46c5c39723fd521c22871f0b6a5371c2653d7e28a6b8611065ffce51dd071acf4f74d959fbe7fb0ed6be1476dc57faf675e8825c86d885c9c503'
