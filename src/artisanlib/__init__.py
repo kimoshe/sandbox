3255dd177fadad6ffa9e92cae5bc8b7822707977
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-18 18:01:15 -0700

    trying on github desktop

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '4.0.3'
-__revision__ = '1'
+__revision__ = '0'
 __build__ = '0'
 __artisan_os__ = 'Linux'
 
 __release_sponsor_name__ = 'artisan.plus'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
-__signature__ = 'ab04f02a9e8b46c5c39723fd521c22871f0b6a5371c2653d7e28a6b8611065ffce51dd071acf4f74d959fbe7fb0ed6be1476dc57faf675e8825c86d885c9c503'
+__signature__ = 'bc5fc03a60824075989389c78af20a879a21f5c0ed6f07000cb1d9dc2fc03245c5f4877c10517760191a7480ca32387e735c6d00155d1c420fcd6823785d6102'
