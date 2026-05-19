4bf2e41434fbe2a6fc4ac45df539b0d3d71b5225
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-19 14:32:25 -0700

    adds git ver to output

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '4.0.3'
-__revision__ = '1'
+__revision__ = ''
 __build__ = '0'
 __artisan_os__ = 'Linux'
 
 __release_sponsor_name__ = 'artisan.plus'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
-__signature__ = 'ab04f02a9e8b46c5c39723fd521c22871f0b6a5371c2653d7e28a6b8611065ffce51dd071acf4f74d959fbe7fb0ed6be1476dc57faf675e8825c86d885c9c503'
+__signature__ = 'e9ca9f1c44ee9e5d4a651dd8938943e410847f5506fa1578b67408f1e91822e089c0be38c78eeb4ba14f8448d1ba1a3f27828e25133cb8da5ca84f01eae6860f'
