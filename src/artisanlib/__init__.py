3fb27203039c0ab0770d468a1e428e0f1ecb34bb
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-19 14:23:00 -0700

    pre-commit permissions reset

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '4.0.3'
-__revision__ = '4'
+__revision__ = '7'
 __build__ = '0'
 __artisan_os__ = 'Linux'
 
 __release_sponsor_name__ = 'artisan.plus'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
-__signature__ = ''
+__signature__ = 'f1aead48d07c470ed816358139f8debb298fa0d7ef1038c251accc61db6e21501d7c249a9c66961a58106c08fc60a8fc1ebb9658e589dd2bf00626b1717a020f'
