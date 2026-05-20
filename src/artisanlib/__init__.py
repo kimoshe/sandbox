8680cb3014e23ae07b22650f31ca9b85b50504fe
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-20 13:08:30 -0700

    message

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '4.0.3'
-__revision__ = '12'
+__revision__ = '11'
 __build__ = '1'
 __artisan_os__ = 'Linux'
 
 __release_sponsor_name__ = 'r1 b1, r12 b1'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
-__signature__ = 'b9d7615e27dbb61826a4382e35ff20692b7a156bc67efd1216e4d1dbfe86562f8ddca6691706d55ed7ad69dd4b9cdb24e3062544a692d24622c386c7a34d7c05'
+__signature__ = 'fd5214a73cbde70ae7a3daa889abb53235188bf66e77a1ac646ddba6abcb87717a05b3cddda5ea6e171e180278a20037000f5f32896be1679a07d5c166c0e402'
