40c3bcf3d8b4b3648fdb01be81a63d46cb4978f1
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-05-20 08:45:56 -0700

    check is not ab04f

--- src/artisanlib/__init__.py
+++ src/artisanlib/__init__.py
@@ -3,7 +3,7 @@ __revision__ = '12'
 __build__ = '12'
 __artisan_os__ = 'Linux'
 
-__release_sponsor_name__ = 'staged r1 b1 ab04, unstaged r12 b12'
+__release_sponsor_name__ = 'staged r1 b1 ab04, unstaged r12 b12 b9d7'
 __release_sponsor_domain__ = 'artisan.plus'
 __release_sponsor_url__ = 'https://artisan.plus/'
 __signature__ = 'b9d7615e27dbb61826a4382e35ff20692b7a156bc67efd1216e4d1dbfe86562f8ddca6691706d55ed7ad69dd4b9cdb24e3062544a692d24622c386c7a34d7c05'
