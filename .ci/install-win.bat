698b6be5a74bbe3165ed0a813ed06ea77f1dd44e
kimoshe <116674739+kimoshe@users.noreply.github.com>
2026-06-20 13:46:04 -0700

    try again

--- .ci/install-win.bat
+++ .ci/install-win.bat
@@ -57,11 +57,11 @@ if not exist %PYTHON_PATH%\Lib\site-packages\libusb_package\libusb-1.0.dll (
 echo ***** Start Install QTLinguist/lrelease.exe
 curl -L -O https://github.com/thurask/Qt-Linguist/releases/download/20260425/linguist_6.11.0.zip
 if not exist linguist_6.11.0.zip (exit /b 98)
-7z x linguist_6.11.0.zip
+7z x linguist_6.11.0.zip -o.\QtLinguist\
 :: Pause Build Here For Remote Desktop Access
-PowerShell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command "if ($env:APPVEYOR_RDP_BLOCK -eq $true) {$blockRdp = $true; & iex ((new-object net.webclient).DownloadString(\"https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1\"))}"
+:: PowerShell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command "if ($env:APPVEYOR_RDP_BLOCK -eq $true) {$blockRdp = $true; & iex ((new-object net.webclient).DownloadString(\"https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1\"))}"
 
-if not exist QtLinguist/lrelease.exe/ (exit /b 99)
+if not exist QtLinguist/lrelease.exe (exit /b 99)
 echo ***** Finished install QTLinguist/lrelease
 
 
